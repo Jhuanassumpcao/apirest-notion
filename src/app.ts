40c22fbb11b65d620c5656177cfa66fcd3913933
Jhuanassumpcao <jhuanassumpcao@gmail.com>
2024-11-11 15:45:19 -0300

    feat: open api connection

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import router from './routes.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
